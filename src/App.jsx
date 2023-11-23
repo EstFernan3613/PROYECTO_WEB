e7df2bcade2f32c08cf3ef4afb495d156eba838c
JoanManuel17 <110619713+JoanManuel17@users.noreply.github.com>
2023-11-23 11:44:47 -0500

    Cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import Home from './Components/Home';
+import Login from './Components/Login';
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [user, setUser] = useState(null);
 
-export default App
+  const handleLogin = (username) => {
+    // Puedes realizar lógica de autenticación aquí y establecer el usuario
+    // Aquí simplemente almacenamos el nombre de usuario en el estado
+    setUser(username);
+  };
+
+  return (
+    <Router>
+      <Switch>
+        <Route path="/login">
+          {user ? <Redirect to="/" /> : <Login onLogin={handleLogin} />}
+        </Route>
+        <Route path="/">
+          {user ? <Home /> : <Redirect to="/login" />}
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
